bbb173c1f4acdae3c767efe3bb1e901d507e8958
blykrise <blykrise@gmail.com>
2024-04-26 17:06:28 +0100

    moves client side

--- src/cli-util/network/client.c
+++
@@ -1,8 +0,0 @@
-#include"client.h"
-#include<stdlib.h>
-#include<stdio.h>
-
-int clientSend(Request *request){
-    printf("Data: %s, length: %d\n", request->data, request->length);
-    return SEND_SUCCESSFUL;
-}
